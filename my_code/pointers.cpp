50b9be2a62aea5c6eba6adfd1719314828ec6666
Calvin <luoyantao99@hotmail.com>
2019-06-04 11:34:10 -0400

    homework2

---
+++ my_code/pointers.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include "pointers.h"
+
+using namespace std;
+
+void inc_by_value(int n) {
+    ++n;
+}
+
+
+void inc_with_pointer(int* iptr) {
+    ++(*iptr);
+}
+
+
+void inc_with_reference(int& n) {
+    ++n;
+}
+
+
+Thing** create_array_of_things(int n) {
+    Thing** tarray = new Thing*[n];
+    for (int i=0; i<n; i++)
+    {
+        tarray[i] = new Thing(i);
+    }
+    return tarray;
+}
+
+void print_all_things(Thing** things, int n) {
+    
+}
+
+void double_all_things(Thing** things, int n) {
+}
+
+void delete_all_things(Thing** things, int n) {
+}
+
+
+void assignTA(Student& s, Student& ta) {
+}
+
+void printTAs(vector<Student> students) {
+}
