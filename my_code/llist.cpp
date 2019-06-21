3a904f87b25118b23cf09fde24c33b2093db27fe
Calvin <luoyantao99@hotmail.com>
2019-06-21 09:55:12 -0400

    llist

---
+++ my_code/llist.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include "llist.h"
+
+using namespace std;
+
+
+
+/*
+ * Output a Node pointer.
+ * */
+ostream& operator<<(ostream& os, const Node* nd) {
+    if (!nd) os << "nullptr";
+    else os << "Data: " << nd->data << " Next: ";
+    return os;
+}
+
+
+/*
+ * Add a node to the end of a list.
+ * */
+void add_at_end(Node*& head, int d) {
+    if (!head) head = new Node(d, nullptr);
+    else add_at_end(head->next, d);
+}
+
+
+/*
+ * Given the head of a list, print the whole thing.
+ * Let's do this recursively!
+ * */
+void print_list(ostream& os, const Node* curr) {
+    os << curr << " ";
+    if (curr) print_list(os, curr->next);
+}
+
+/*
+ * Add a node at the front of a linked list.
+ * */
+void add_at_front(Node*& head, int d) {
+    head = new Node(d, head);
+}
+
+
+/*
+ * Get the last node of a list.
+ * */
+Node* last(Node* head) {
+    if (!head) return nullptr;
+    while (head->next != nullptr)
+    {
+        head = head->next;
+    }
+    return head;
+}
